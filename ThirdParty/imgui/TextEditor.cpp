1a29b9c381919f39535c37057d70ef1098985920
Dominik 'squ@ll' Jasiński <dominikjasinski@o2.pl>
2019-05-20 14:17:55 +0200

    TextEditor color palette improvements

--- ThirdParty/imgui/TextEditor.cpp
+++ ThirdParty/imgui/TextEditor.cpp
@@ -1616,25 +1616,25 @@ const TextEditor::Palette & TextEditor::GetDarkPalette()
 	const static Palette p = { {
 		0xff7f7f7f,	// Default
 		0xffd69c56,	// Keyword	
-		0xff00ff00,	// Number
-		0xff7070e0,	// String
+		0xff7070e0,	// Number
+		0xff006ED6,	// String
 		0xff70a0e0, // Char literal
 		0xffffffff, // Punctuation
 		0xff408080,	// Preprocessor
 		0xffaaaaaa, // Identifier
 		0xff9bc64d, // Known identifier
 		0xffc040a0, // Preproc identifier
-		0xff206020, // Comment (single line)
-		0xff406020, // Comment (multi line)
-		0xff101010, // Background
+		0xff0E8E0E, // Comment (single line)
+		0xff0E8E0E, // Comment (multi line)
+		0xff493519, // Background
 		0xffe0e0e0, // Cursor
-		0x80a06020, // Selection
+		0xccf06020, // Selection
 		0x800020ff, // ErrorMarker
 		0x40f08000, // Breakpoint
 		0xff707000, // Line number
-		0x40000000, // Current line fill
+		0xA062461f, // Current line fill
 		0x40808080, // Current line fill (inactive)
-		0x40a0a0a0, // Current line edge
+		0x4000D8FF, // Current line edge
 	} };
 	return p;
 }
